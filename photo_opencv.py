3934edd2e7fb4b1a6ef8a06e2df5143245a34c3a
Ann19941118 <40021661+Ann19941118@users.noreply.github.com>
2019-03-10 11:19:45 +0800

    Add files via upload

---
+++ photo_opencv.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+while(1):
+    # get a frame
+    ret, frame = cap.read()
+    # show a frame
+    cv2.imshow("capture", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite("./pic_tmp/test1.jpg", frame)
+        break
+cap.release()
+cv2.destroyAllWindows()
